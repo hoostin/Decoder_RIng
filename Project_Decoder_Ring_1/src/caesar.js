c06e6e974be57bca59374c69fa73978d9a91dd1d
Austin Mckee <captainlantern8@gmail.com>
2021-02-08 09:10:07 -0800

    start

--- Project_Decoder_Ring_1/src/caesar.js
+++ Project_Decoder_Ring_1/src/caesar.js
@@ -5,14 +5,36 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
-
+  /**
+   *
+   * @param {string} input string message to encrypt or decrypt
+   * @param {number} shift refers to how much each letter is "shifted" by.
+   * A positive number means shifting to the right (i.e. "A" becomes "D")
+   * whereas a negative number means shifting to the left (i.e. "M" becomes "K").
+   * @param {boolean} encode encode or decode message true =encode false = decode
+   */
+  // 97 -122 unicode letters lowercase
   function caesar(input, shift, encode = true) {
     // your solution code here
+    if (shift === 0 || shift === Math.abs(shift) > 25) {
+      return false;
+    }
+    input = input.toLowerCase();
+    for (let i = 0; i < input.length; i++) {
+      const originalChar = input.charCodeAt(i);
+      if (originalChar < 123 && originalChar > 96) {
+        // check what orignalChar + shift is if higher than 123 or lower than 96 means wrap
+        // might use % to find remainder for wrapping
+        // that value will be newChar
+        // use newChar with
+      }
+    }
   }
 
   return {
     caesar,
   };
 })();
+caesarModule.caesar("test", 1);
 
 module.exports = caesarModule.caesar;
