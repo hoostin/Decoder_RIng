c4fb5b6db7a16d6952701dab09ac5434851eec83
Austin Mckee <captainlantern8@gmail.com>
2021-02-09 07:54:33 -0800

    poly - getCode functional

--- Project_Decoder_Ring_1/src/polybius.js
+++ Project_Decoder_Ring_1/src/polybius.js
@@ -5,7 +5,49 @@
 
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
+  // letternum = (input.charCodeAt(i) -96)
 
+  /*
+    if letternum  > 10 
+    {
+      letternum -=1
+    }
+    if letternum% 5 !=0 
+    row = (letternum / 5) +1
+    
+    else 
+    row = letternum/5
+
+    column = 5-(letternum %5)
+
+    without accounting for array index being 0 and i/j stuff
+    */
+  /**
+   * function takes in letter and returns two number string
+   * @param {string} letter single letter
+   */
+  function getCode(letter) {
+    let row = 0;
+    let column = 0;
+    letter = letter.toLowerCase();
+    let letterNum = letter.charCodeAt(0) - 96;
+    if (letterNum >= 10) {
+      letterNum -= 1;
+    }
+
+    if (letterNum % 5 != 0) {
+      row = Math.floor(letterNum / 5) + 1;
+      column = letterNum % 5;
+    } else {
+      row = Math.floor(letterNum / 5);
+      column = 5;
+    }
+    // 5 -
+    return `${column}${row}`;
+  }
+  function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+  }
   function polybius(input, encode = true) {
     // your solution code here
 
@@ -19,19 +61,7 @@ const polybiusModule = (function () {
     ];
 
     // letter value out of 25(26) fix i/j
-    // letter =
 
-    /*
-    if letternum% 5 !=0 
-    row = (letternum / 5) +1
-    
-    else 
-    row = letternum/5
-
-    column = 5-(letternum %5)
-
-    without accounting for array index being 0 and i/j stuff
-    */
     // use find to encode
     // use string splitter thing or indexes to get both array numbers to decode
     //
@@ -40,7 +70,14 @@ const polybiusModule = (function () {
 
   return {
     polybius,
+    isLetter,
+    getCode,
   };
 })();
-
+const testString = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+for (let c of testString) {
+  if (polybiusModule.isLetter(c)) {
+    console.log(polybiusModule.getCode(c));
+  }
+}
 module.exports = polybiusModule.polybius;
